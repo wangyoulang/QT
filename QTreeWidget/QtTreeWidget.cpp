831e91bc00d72e000885a14f96d1b1d9da824fcb
wangyoulang <49062714+wangyoulang@users.noreply.github.com>
2023-06-26 16:50:33 +0800

    Update QtTreeWidget.cpp

--- QTreeWidget/QtTreeWidget.cpp
+++ QTreeWidget/QtTreeWidget.cpp
@@ -17,11 +17,11 @@ QtTreeWidget::QtTreeWidget(QWidget *parent)
 
 
     // 添加节点方法1
-    QTreeWidgetItem* group1 = new QTreeWidgetItem(ui->treeWidget);
+    QTreeWidgetItem* group1 = new QTreeWidgetItem(ui->treeWidget, 1); // 1表示树节点type，类型为int
     group1->setText(0, "第一组");
     //group1->setFlags(Qt::ItemIsUserCheckable | Qt::ItemIsEnabled | Qt::ItemIsSelectable);
     //group1->setCheckState(0, Qt::Unchecked); //状态为未选中
-    QTreeWidgetItem* yuwen = new QTreeWidgetItem();
+    QTreeWidgetItem* yuwen = new QTreeWidgetItem(2);
     yuwen->setText(0, "语文");
     group1->addChild(yuwen);
     //yuwen->setFlags(Qt::ItemIsUserCheckable | Qt::ItemIsEnabled | Qt::ItemIsSelectable);
@@ -57,4 +57,43 @@ void QtTreeWidget::on_item_double_clicked(QTreeWidgetItem* item, int colum)
     {
         QMessageBox::information(0, "提示", QString("%1没有子节点").arg(item->text(0)));
     }
+}
+
+// 鼠标右击事件
+void QtTreeWidget::contextMenuEvent(QContextMenuEvent* event)
+{
+    QPoint pos = event->globalPos();  // 获取全局坐标
+    QPoint widgetPos = ui->treeWidget->mapFromGlobal(pos); // 将全局坐标转换为相对于树部件的坐标
+    QTreeWidgetItem* item = ui->treeWidget->itemAt(widgetPos);  // 获取鼠标位置下的树节点
+    /*
+    菜单
+    */
+    designTable = new QAction("第一组");
+    designTable->setStatusTip("第一组");
+    closeConnection = new QAction("语文");
+    closeConnection->setStatusTip("语文");
+    globle = new QAction("全局");
+    globle->setStatusTip("全局");
+    connect(designTable, &QAction::triggered, this, &QtTreeWidget::add_designTable);
+    connect(closeConnection, &QAction::triggered, this, &QtTreeWidget::add_closeConnection);
+    connect(globle, &QAction::triggered, this, &QtTreeWidget::add_globle);
+
+    menu->clear();
+    if (item == nullptr)
+    {
+        menu->addAction(globle);
+        menu->exec(QCursor::pos());
+        return;
+    }
+    int type = item->type();
+    if (type == 1)
+    {
+        menu->addAction(designTable);
+    }
+    else if (type == 2)
+    {
+        menu->addAction(closeConnection);
+    }
+    menu->exec(QCursor::pos());
+    //menu->exec(ui->treeWidget->mapToGlobal(pos));
 }
